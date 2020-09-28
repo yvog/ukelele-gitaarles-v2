9503a3a06b6d2eff862d34b68c35f2a1e200699d
Yvo <18664669+yvog@users.noreply.github.com>
2020-09-28 15:36:39 +0200

    TypeScript initialization

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,34 +3,33 @@ import Head from 'next/head'
 
 export default function Layout({
   children,
-  title = 'Ukelele-Gitaarles',
+  title = 'Ukelele-Gitaarles 2.0',
 }) {
   return (
     <div>
       <Head>
-        <title>{title}</title>
         <meta charSet="utf-8" />
+        <title>{title}</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no,date=no" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <header>
         <nav>
           <Link href="/">
             <a>Home</a>
-          </Link>{' '}
-          |
-          <Link href="/about">
-            <a>About</a>
-          </Link>{' '}
-          |
-          <Link href="/contact">
-            <a>Contact</a>
           </Link>
         </nav>
       </header>
 
       {children}
 
-      <footer><div className="example">Hello World!</div></footer>
+      <footer>
+        <div className="example">Hello World!</div>
+      </footer>
+
     </div>
   )
 }
