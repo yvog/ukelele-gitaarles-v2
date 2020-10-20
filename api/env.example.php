aaa01262f101f056d1e9387d546eda3e6056a498
Yvo <18664669+yvog@users.noreply.github.com>
2020-10-20 14:32:09 +0200

    CostsCalculator component

---
+++ api/env.example.php
@@ -0,0 +1,4 @@
+<?php
+
+$_ENV['API_KEY'] = '123abc';
+$_ENV['HOME_ADDRESS'] = '1234+AB+The+Best+City+Nederland';
--- components/button/button.tsx
+++ components/button/button.tsx
@@ -5,16 +5,18 @@ type ButtonComponentProps = {
     filled?: boolean;
     children?: React.ReactNode;
     href: string;
+    disabled?: boolean;
     onClickHandler: (e: any) => void;
 };
 const ButtonComponent: React.FC<ButtonComponentProps> = ({
     children,
     filled = false,
     href,
+    disabled = false,
     onClickHandler
 }) => {
     return (
-        <a onClick={onClickHandler} href={href} className={classNames(styles.button, {[styles.button_filled]: filled})}>
+        <a onClick={onClickHandler} href={href} className={classNames(styles.button, {[styles.filled]: filled}, {[styles.disabled]: disabled})}>
             {children}
         </a>
     );
