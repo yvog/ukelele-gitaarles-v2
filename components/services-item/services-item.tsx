88da0625f69caf6ef3cf61eccc38b3ea8ad8d445
Yvo Geldhof <yvogeldhof@MBP-van-Yvo-2.lan>
2021-01-13 19:57:28 +0100

    Removed Link components

--- components/services-item/services-item.tsx
+++ components/services-item/services-item.tsx
@@ -1,37 +1,34 @@
-
-import classNames from 'classnames';
-import Link from 'next/link';
-import styles from './services-item.module.scss';
+import classNames from "classnames";
+import Link from "next/link";
+import styles from "./services-item.module.scss";
 
 type ServicesItemComponentProps = {
-    src: string;
-    firstTitle: string;
-    secondTitle: string;
-    url: string;
+  src: string;
+  firstTitle: string;
+  secondTitle: string;
+  url: string;
 };
 
 const ServicesItemComponent: React.FC<ServicesItemComponentProps> = ({
-    src,
-    firstTitle,
-    secondTitle,
-    url
+  src,
+  firstTitle,
+  secondTitle,
+  url,
 }) => {
-    return (
-        <>
-            <div className={classNames('col-lg-4')}>
-                <Link href={url}>
-                    <a className={styles.services_item}>
-                        <img src={src} width="300" height="200" alt={src} />
-                        <h3>
-                            {firstTitle}
-                            <span className={styles.divider}></span>
-                            {secondTitle}
-                        </h3>
-                    </a>
-                </Link>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={classNames("col-lg-4")}>
+        <a href={url} className={styles.services_item}>
+          <img src={src} width="300" height="200" alt={src} />
+          <h3>
+            {firstTitle}
+            <span className={styles.divider}></span>
+            {secondTitle}
+          </h3>
+        </a>
+      </div>
+    </>
+  );
 };
 
 export const ServicesItem = ServicesItemComponent;
