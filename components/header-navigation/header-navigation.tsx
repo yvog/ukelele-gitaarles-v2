f968ff3a4b408511905b37259a2cd37c136ec65b
Yvo Geldhof <yvogeldhof@MBP-van-Yvo-2.lan>
2021-01-13 19:57:28 +0100

    Removed Link components

--- components/header-navigation/header-navigation.tsx
+++ components/header-navigation/header-navigation.tsx
@@ -1,83 +1,66 @@
-import classNames from 'classnames';
-import Link from 'next/link';
-import React, {useCallback, useRef, useState} from 'react';
-import styles from './header-navigation.module.scss';
-import {MenuButton} from '../menu-button/menu-button';
+import classNames from "classnames";
+import Link from "next/link";
+import React, { useCallback, useRef, useState } from "react";
+import styles from "./header-navigation.module.scss";
+import { MenuButton } from "../menu-button/menu-button";
 
 type HeaderNavigationComponentProps = {
-  variant?: 'white' | 'black';
+  variant?: "white" | "black";
 };
 
 const HeaderNavigationComponent: React.FC<HeaderNavigationComponentProps> = ({
-  variant = 'white'
+  variant = "white",
 }) => {
-  const isBlack = (variant === 'black');
+  const isBlack = variant === "black";
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   const onMenuOpen = () => {
     if (!menuOpen) {
-      setMenuOpen(true)
+      setMenuOpen(true);
     } else {
-      setMenuOpen(false)
+      setMenuOpen(false);
     }
-  }
+  };
 
-  const menuButtonLabel = menuOpen ? 'Close' : 'Menu'
+  const menuButtonLabel = menuOpen ? "Close" : "Menu";
 
   return (
-    <header className={classNames(styles.header, {[styles.black]: isBlack})}>
+    <header className={classNames(styles.header, { [styles.black]: isBlack })}>
+      <a href="/">
+        <img
+          className={styles.logo}
+          src={`/images/logo/ugl_logo_${variant}.svg`}
+          alt="UGL logo white"
+          width="280"
+          height="100"
+        />
+      </a>
 
-      <Link href="/">
-        <a>
-          <img className={styles.logo} src={`/images/logo/ugl_logo_${variant}.svg`} alt="UGL logo white" width="280" height="100" />
-        </a>
-      </Link>
-
-      <nav className={
-        classNames(
-          styles.nav,
-          {
-            [styles.line_hover_nav_black]: isBlack,
-            [styles.line_hover_nav]: !isBlack,
-            [styles.show]: menuOpen
-          }
-        )}>
-
+      <nav
+        className={classNames(styles.nav, {
+          [styles.line_hover_nav_black]: isBlack,
+          [styles.line_hover_nav]: !isBlack,
+          [styles.show]: menuOpen,
+        })}
+      >
         <div className={styles.nav_inner}>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-
-          <Link href="/over-mij">
-            <a>Over mij</a>
-          </Link>
-
-          <Link href="/aanbod">
-            <a>Aanbod</a>
-          </Link>
-
-          <Link href="/leskosten">
-            <a>Leskosten</a>
-          </Link>
-
-          <Link href="/aanvragen">
-            <a>Reparatie / onderhoud</a>
-          </Link>
-
-          <Link href="/aanmelden">
-            <a>Aanmelden</a>
-          </Link>
-
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
+          <a href="/">Home</a>
+          <a href="/over-mij">Over mij</a>
+          <a href="/aanbod">Aanbod</a>
+          <a href="/leskosten">Leskosten</a>
+          <a href="/aanvragen">Reparatie / onderhoud</a>
+          <a href="/aanmelden">Aanmelden</a>
+          <a href="/contact">Contact</a>
         </div>
       </nav>
 
-      <MenuButton icon={`/images/icon/icon_${menuOpen ? 'close' : 'menu'}.svg`} label={menuButtonLabel} onMenuOpen={onMenuOpen} />
-
+      <MenuButton
+        icon={`/images/icon/icon_${menuOpen ? "close" : "menu"}.svg`}
+        label={menuButtonLabel}
+        onMenuOpen={onMenuOpen}
+      />
     </header>
-  )
+  );
 };
 
 export const HeaderNavigation = HeaderNavigationComponent;
