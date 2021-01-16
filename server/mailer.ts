fae969cb60d63faa3feb3c53fd825029cc77806e
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-01-16 22:25:56 +0100

    yo

--- server/mailer.ts
+++ server/mailer.ts
@@ -1,26 +1,49 @@
-const sgMail = require('@sendgrid/mail')
+const nodemailer = require('nodemailer')
+const sgTransport = require('nodemailer-sendgrid-transport')
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.sendgrid.net',
+  port: 465,
+  secure: true,
+  auth: {
+    user: 'apikey',
+    pass: process.env.SENDGRID_API_KEY
+  }
+})
 
 export const sendMail = (subject, text) => {
-  // return new Promise((resolve, reject) => {
-  //   transporter.sendMail(message, (error, info) =>
-  //     error ? reject(error) : resolve(info)
-  //   )
-  // })
-
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-  const from = `${'ukelele-gitaarles.nl'} <${process.env.FROM_EMAIL}>`
-  const mail = {
+  const from = `${'ukelele-gitaarles.nl'} <${process.env.FROM_EMAIL}>`;
+  const message = {
+    from,
     to: process.env.ADMIN_EMAIL,
-    from: from,
-    subject: subject,
-    text: text,
+    subject,
+    text,
     replyTo: from
   }
 
-  sgMail.send(mail).then(() => {
-    console.log('mail sent')
-  }).catch((error) => {
-    console.error(error)
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(message, (error, info) =>
+      error ? reject(error) : resolve(info)
+    )
   })
 }
+
+
+// export const sendMail = (subject, text) => {
+//   // sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+//   // const from = `${'ukelele-gitaarles.nl'} <${process.env.FROM_EMAIL}>`
+//   // const mail = {
+//   //   to: process.env.ADMIN_EMAIL,
+//   //   from: from,
+//   //   subject: subject,
+//   //   text: text,
+//   //   replyTo: from
+//   // }
+
+//   // sgMail.send(mail).then(() => {
+//   //   console.log('mail sent')
+//   // }).catch((error) => {
+//   //   console.error(error)
+//   // })
+// }
