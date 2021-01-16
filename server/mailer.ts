cb773b1f1668bad53be069566351e9e7be9355e4
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-01-16 22:17:49 +0100

    testje

--- server/mailer.ts
+++ server/mailer.ts
@@ -1,27 +1,26 @@
-const nodemailer = require('nodemailer')
-const sgTransport = require('nodemailer-sendgrid-transport')
-
-const transporter = nodemailer.createTransport(sgTransport({
-  host: 'smtp.sendgrid.net',
-  port: 25,
-  auth: {
-    api_key: process.env.SENDGRID_API_KEY
-  }
-}))
+const sgMail = require('@sendgrid/mail')
 
 export const sendMail = (subject, text) => {
-  const from = `${'ukelele-gitaarles.nl'} <${process.env.FROM_EMAIL}>`;
-  const message = {
-    from,
+  // return new Promise((resolve, reject) => {
+  //   transporter.sendMail(message, (error, info) =>
+  //     error ? reject(error) : resolve(info)
+  //   )
+  // })
+
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+  const from = `${'ukelele-gitaarles.nl'} <${process.env.FROM_EMAIL}>`
+  const mail = {
     to: process.env.ADMIN_EMAIL,
-    subject,
-    text,
+    from: from,
+    subject: subject,
+    text: text,
     replyTo: from
   }
 
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(message, (error, info) =>
-      error ? reject(error) : resolve(info)
-    )
+  sgMail.send(mail).then(() => {
+    console.log('mail sent')
+  }).catch((error) => {
+    console.error(error)
   })
 }
