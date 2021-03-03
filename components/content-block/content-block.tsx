f5839c3819b34e31c19e1997571669978065008e
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-03-03 16:39:47 +0100

    contentblock swap on mobile

--- components/content-block/content-block.tsx
+++ components/content-block/content-block.tsx
@@ -5,13 +5,19 @@ import styles from './content-block.module.scss'
 type ContentBlockComponentProps = {
   title: string
   img?: React.ReactNode
+  swapOnMobile?: boolean
 }
 
-const ContentBlockComponent: React.FC<PropsWithChildren<ContentBlockComponentProps>> = ({ title, children, img }) => {
+const ContentBlockComponent: React.FC<PropsWithChildren<ContentBlockComponentProps>> = ({
+  title,
+  children,
+  img,
+  swapOnMobile,
+}) => {
   return (
     <section className={classNames(styles.content_block, 'main-container body')}>
       <h2>{title}</h2>
-      <div className={styles.content_block_row}>
+      <div className={classNames(styles.content_block_row, { [styles.swapOnMobile]: swapOnMobile })}>
         <div
           className={classNames(styles.content_block_row_content, {
             [styles.no_image]: img === undefined,
