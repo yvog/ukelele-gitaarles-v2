ab477a4805c514ba705755e36d4dae06ff274bca
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-12-24 09:01:29 +0100

    fix: no inline svg images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const withImages = require('next-images')
 
 module.exports = withImages({
+  inlineImageLimit: false,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
