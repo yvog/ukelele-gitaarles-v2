e294b41df3c58e34ee0037ca4c5603b071d34a68
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2021-12-25 11:05:57 +0100

    fix: imagemin tool

--- imagemin.js
+++ imagemin.js
@@ -2,8 +2,9 @@ const imagemin = require('imagemin')
 const imageminWebp = require('imagemin-webp')
 
 ;(async () => {
-  const files = await imagemin(['public/**/*.{jpg,png}'], {
+  await imagemin(['public/**/*.{jpg,png}'], {
     destination: '/imagemin',
     plugins: [imageminWebp({ quality: 100 })],
   })
+  console.log('Images optimized')
 })()
