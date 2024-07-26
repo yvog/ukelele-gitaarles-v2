dd9708243d3664583f02adab30d83d526812d5f8
Yvo Geldhof <18664669+yvog@users.noreply.github.com>
2024-07-26 09:21:15 +0200

    updated preview banner with button to close preview mode

--- src/components/preview-banner/preview-banner.tsx
+++ src/components/preview-banner/preview-banner.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import styles from './preview-banner.module.scss';
+import Link from 'next/link';
 
 type PreviewBannerProps = unknown;
 
 export const PreviewBanner: React.FC<PreviewBannerProps> = () => {
   return (
     <div className={styles.preview_banner}>
-      Je bekijkt de huidige pagina in preview modus.
+      <p>Je bekijkt de huidige pagina in preview modus.</p>
+      <Link href="/api/preview/clear" passHref>
+        <a className="link">Sluit preview</a>
+      </Link>
     </div>
   );
 };
